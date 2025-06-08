3d3a2b0a221b76d8e626d4487dc2ade0b1b26163
Kimaru <kevinbogonko430@gmail.com>
2025-06-08 18:42:56 +0300

    Corrected env variable

--- src/hooks/api.js
+++ src/hooks/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const baseURL =  import.meta.env.DEV ? "/api" : import.meta.env.VITE_PRODUCTION_BACKEND_BASE_URL + "/api" // Use proxy in development
+const baseURL = import.meta.env.DEV
+  ? "/api"
+  : import.meta.env.VITE_BACKEND_BASE_URL + "/api"; // Use proxy in development
 
 const API = axios.create({
   baseURL,
